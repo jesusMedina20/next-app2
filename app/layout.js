c8e3202c0e8bf73d2fa3fff9bc4a7ba027aaec70
jesus medina <jesus.amb0241@gmail.com>
2023-01-02 11:26:54 -0400

    add about pages

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,35 @@
+import Link from "next/link";
+const links = [
+  {
+    label: "home",
+    route: "/",
+  },
+  {
+    label: "about",
+    route: "/about",
+  },
+];
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <head>
+        <title>my first app with next js 13</title>
+      </head>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              {links.map(({ label, route }) => (
+                <li key={route}>
+                  <Link href={route}>{label}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
