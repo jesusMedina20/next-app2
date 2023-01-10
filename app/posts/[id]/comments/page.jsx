119cc806adacac6a4dc3dab0eef7d4873dafd76d
jesus medina <jesus.amb0241@gmail.com>
2023-01-10 12:42:57 -0400

    add comments page

---
+++ app/posts/[id]/comments/page.jsx
@@ -0,0 +1,23 @@
+const fetchComments = (id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
+    next: {
+      revalidate: 60,
+    },
+  }).then((res) => res.json());
+};
+
+export default async function Post({ params }) {
+  const { id } = params;
+  const comments = await fetchComments(id);
+
+  return (
+    <ul>
+      {comments.map((comment) => (
+        <li key={comment.id}>
+          <h2>{comment.name}</h2>
+          <p>{comment.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
